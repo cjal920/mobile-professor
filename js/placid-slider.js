39ac695ecacc5501377d67f4d3aa0dc095544184
Colin Larkin <colinlarkin@Colins-MacBook-Air.local>
2020-04-01 17:19:45 -0400

    adding testimonials

---
+++ js/placid-slider.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function() {
+    jQuery(".placid_slider_fouc .placid_slider_set").show();
+ });jQuery(document).ready(function(){
+     jQuery("a[rel^='prettyPhoto']").prettyPhoto({deeplinking: false,social_tools:false});
+ });
