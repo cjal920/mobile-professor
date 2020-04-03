34ee9fad9b5fe56b30a0e4fa997b43bc73cbaf39
Colin Larkin <colinlarkin@Colins-MacBook-Air.local>
2020-04-02 21:21:45 -0400

    added placid slider

--- js/placid-slider.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function() {
-    jQuery(".placid_slider_fouc .placid_slider_set").show();
- });jQuery(document).ready(function(){
-     jQuery("a[rel^='prettyPhoto']").prettyPhoto({deeplinking: false,social_tools:false});
- });
